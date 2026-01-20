d53e90e712937062c08c4d360cbfe4e4aa55d814
sameershaikh126878 <ywallah6@gmail.com>
2026-01-20 13:35:53 +0530

    Change API_ID and API_HASH in vars.py
    Updated API_ID and API_HASH values in vars.py.

--- vars.py
+++ vars.py
@@ -3,9 +3,10 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "23274330"))
-API_HASH = environ.get("API_HASH", "970e2e79779707c56d2b453b3a6eea48")
+API_ID = int(environ.get("API_ID", "21437108"))
+API_HASH = environ.get("API_HASH", "53b13c686a0cce30a8aa35710ac11a62")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(environ.get("OWNER", "7431004177"))
+OWNER = int(environ.get("OWNER", "8447066074"))
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
